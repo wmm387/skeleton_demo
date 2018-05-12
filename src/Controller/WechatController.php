e2f87f527090e21a9cd79ca1f91bd9b480c2b4ef
wmm387 <wangyuanwmm@163.com>
2018-05-12 10:08:42 +0800

    测试模板消息

--- src/Controller/WechatController.php
+++ src/Controller/WechatController.php
@@ -18,13 +18,32 @@ class WechatController extends Controller
 		'secret' => '294fe7ec5853bf407664973fa4d580b0',
 
 		'response_type' => 'array',
+
+        'log' => [
+            'level' => 'debug',
+            'file' => __DIR__.'/wechat.log',
+        ],
 	];
 
 	$app = Factory::officialAccount($config);
 
-	$app->server->push(function ($message) {
-		return "hello world!";
-	});
+//	$app->server->push(function ($message) {
+//		return "hello world!";
+//	});
+
+//    $app->template_message->getPrivateTemplates();
+
+    $app->template_message->send([
+        'touser' => 'oIwts0qGX0bcNkQw9lv0zz0wtGDg',
+        'template_id' => 'RFTuKAc3efCU2Yjev2wzngZJ3brix8ymYFy8T0u2q6A',
+        'url' => 'https://www.baidu.com',
+        'data' => [
+            'key1' => 'key1',
+            'key2' => 'key2',
+            'key3' => 'key3',
+            'key4' => 'key4',
+        ],
+    ]);
 
 	$response = $app->server->serve();
 
